5423e724c4ce3acafeb99683b8e467d5bd0add6c
Shakhawat2 <shohugmahbub@gmail.com>
2022-11-24 12:26:57 +0600

    create private route

--- src/Routes/PrivateRoutes/PrivateRoutes.js
+++ src/Routes/PrivateRoutes/PrivateRoutes.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom/dist';
+import { AuthContext } from '../../Context/UserContext';
+import LargeLoader from '../../Pages/Shared/Loader/LargeLoader/LargeLoader';
 
-const PrivateRoutes = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const PrivateRoutes = ({children}) => {
+    const {user, loading} = useContext(AuthContext);
+    const location = useLocation()
+    if(loading){
+        return <LargeLoader></LargeLoader>
+    }
+
+    if(user && user?.uid){
+        return children;
+    }
+    return <Navigate to='/login' state={{ from: location }} replace ></Navigate>
+    
 };
 
 export default PrivateRoutes;
