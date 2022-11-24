58295f18b5c154ac48858bc6c6e3fe3210e68a6d
Shakhawat2 <shohugmahbub@gmail.com>
2022-11-24 11:03:28 +0600

    install react-router-dom and create a router

--- src/Routes/Router/Router.js
+++ src/Routes/Router/Router.js
@@ -11,7 +11,7 @@ export const router = createBrowserRouter([
             {
                 path : '/',
                 element : <ErrorPage></ErrorPage>
-            }
+            },
         ]
     }
 ])
